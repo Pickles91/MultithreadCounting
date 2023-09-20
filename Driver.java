a20ee2b1d52f074a483d779ce03926604952e78c
Stephan Sommer <steven.sommer@go.winona.edu>
2023-09-20 18:00:55 -0500

    Add files via upload

---
+++ Driver.java
@@ -0,0 +1,85 @@
+package Question4;
+
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int nThreads = 0;
+		int upperBound = 0;
+		
+		// Create an instance of Scanner named sc
+		Scanner sc = new Scanner(System.in);
+		
+		while (true) {
+			try {
+				System.out.print("Enter the upper bound (N): ");
+				upperBound = sc.nextInt();
+				
+				if (upperBound < Math.pow(10, 6)|| upperBound > Math.pow(10, 9)) {
+					throw new NumberFormatException();
+				}
+				
+				System.out.print("Enter the number of threads (1-10): ");
+				nThreads = sc.nextInt();
+				
+				if (nThreads < 1 || nThreads > 10) {
+					throw new NumberFormatException();
+				}
+				
+				break;
+			} catch (NumberFormatException e) {
+				System.out.println("Invalid input. Please enter valid values.");
+				System.out.println("");
+			}
+		}
+		
+		long startTime = System.currentTimeMillis();
+		
+		// Create an executor service with a fixed thread pool
+		ExecutorService executorService = Executors.newFixedThreadPool(nThreads);
+		
+		List<Future<Integer>> futures = new ArrayList<>();
+		
+		// Split the task into sub task and submit them to the thread pool
+		int blockSize = upperBound / nThreads;
+		int lower = 1;
+		int upper;
+		
+		for (int i = 0; i < nThreads; i++) {
+			upper = i == nThreads - 1 ? upperBound : lower + blockSize - 1;
+			Task task = new Task(lower, upper);
+			futures.add(executorService.submit(task));
+			lower = upper + 1;
+		}
+		
+		// Collect partial counts from each thread
+		int totalCount = 0;
+		for (Future<Integer> future : futures) {
+			try {
+				totalCount += future.get();
+			
+			} catch (InterruptedException | ExecutionException e) {
+                e.printStackTrace();
+            }
+			
+		}
+		
+		// Shutdown the executor service
+		executorService.shutdown();
+		
+		long endTime = System.currentTimeMillis();
+		long executionTime = endTime - startTime;
+		
+		// Display results
+		System.out.println("Total perfect numbers between 1 and " + upperBound + ": " + totalCount);
+		System.out.println("Execution time: " + executionTime + " ms");
+	}
+
+}
---
+++ Task.java
@@ -0,0 +1,36 @@
+package Question4;
+
+import java.util.concurrent.Callable;
+
+// use two thread to calculate the sum of squared root from 1 to N
+public class Task implements Callable<Integer> {
+	private int lowerBound, upperBound;
+	
+	public Task(int lower, int upper ) {
+		this.lowerBound = lower;
+		this.upperBound = upper;
+	}
+	
+	public Integer call() {
+		int count = 0;
+		for (int i = lowerBound; i <= upperBound; i++) {
+			if (isPerfectNumber(i)) {
+				count++;
+			}
+		}
+		return count;
+	}
+	
+	private boolean isPerfectNumber(int num) {
+		int sum = 1;
+		for (int i = 2; i * i <= num; i++) {
+			if (num % 1 == 0) {
+				sum += i;
+				if ( i != num / i) {
+					sum += num / i;
+				}
+			}
+		}
+		return sum == num;
+	}
+}
