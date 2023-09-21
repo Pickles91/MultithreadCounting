1de7f99ff119f71966d82e11f9dea9a172de0fc7
Stephan Sommer <steven.sommer@go.winona.edu>
2023-09-20 23:05:07 -0500

    Update Task.java

--- Task.java
+++ Task.java
@@ -24,7 +24,7 @@ public class Task implements Callable<Integer> {
 	private boolean isPerfectNumber(int num) {
 		int sum = 1;
 		for (int i = 2; i * i <= num; i++) {
-			if (num % 1 == 0) {
+			if (num % i == 0) {
 				sum += i;
 				if ( i != num / i) {
 					sum += num / i;
